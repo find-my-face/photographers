373140d713114cb101774ba9385eef0d96261cf0
andrew <burns.andrew@hotmail.co.uk>
2019-07-09 14:40:33 +0100

    password forget component created

--- src/components/PasswordForget/PasswordForgetIndex.js
+++ src/components/PasswordForget/PasswordForgetIndex.js
@@ -1,11 +1,68 @@
-import React from "react";
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { withFirebase } from "../Firebase/FirebaseIndex";
+import * as ROUTES from "../../constants/routes";
 
-const PasswordForgetIndex = () => {
-  return (
-    <div>
-      <h1>hello from PasswordForgetIndex </h1>
-    </div>
-  );
-};
+const PasswordForgetPage = () => (
+  <div>
+    <h1>Password Forget</h1>
+    <PasswordForgetForm />
+  </div>
+);
 
-export default PasswordForgetIndex;
+class PasswordForgetFormBase extends Component {
+  state = {
+    email: "",
+    error: null
+  };
+
+  onSubmit = event => {
+    const { email } = this.state;
+
+    this.props.firebase
+      .doPasswordReset(email)
+      .then(() => {
+        this.setState({ email });
+      })
+      .catch(error => {
+        this.setState({ error });
+      });
+    event.preventDefault();
+  };
+
+  onChange = event => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
+
+  render() {
+    const { email, error } = this.state;
+    const isInvalid = email === "";
+
+    return (
+      <form onSubmit={this.onSubmit}>
+        <input
+          name="email"
+          value={email}
+          onChange={this.onChange}
+          type="text"
+          placeholder="Email Address"
+        />
+        <button disabled={isInvalid} type="submit">
+          Reset My Password
+        </button>
+        {error && <p>{error.message}</p>}
+      </form>
+    );
+  }
+}
+
+const PasswordForgetLink = () => (
+  <p>
+    <Link to={ROUTES.PASSWORD_FORGET}>Forgot Password?</Link>
+  </p>
+);
+
+export default PasswordForgetPage;
+const PasswordForgetForm = withFirebase(PasswordForgetFormBase);
+
+export { PasswordForgetForm, PasswordForgetLink };
