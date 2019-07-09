e3a90897d4eae40d784e72e6a54ea0b1075723c0
andrew <burns.andrew@hotmail.co.uk>
2019-07-09 16:36:34 +0100

    added routes so components have different paths and pages

--- src/components/SignUp/SignUpIndex.js
+++ src/components/SignUp/SignUpIndex.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { Link, withRouter } from "react-router-dom";
 import { compose } from "recompose";
 import { withFirebase } from "../Firebase/FirebaseIndex";
-import { FirebaseContext } from "../Firebase/FirebaseIndex";
+// import { FirebaseContext } from "../Firebase/FirebaseIndex";
 
 import * as ROUTES from "../../constants/routes";
 
