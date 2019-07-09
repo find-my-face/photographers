a5fb31a5f00c83daca008e7b52422ea70b85b5d4
andrew <burns.andrew@hotmail.co.uk>
2019-07-09 10:10:10 +0100

    file structure built to include components and constants folders

---
+++ src/components/App/AppIndex.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import "../../App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>find my face. Coming soon.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
-import App from "./App";
-import Firebase, { FirebaseContext } from "./components/firebase";
+import App from "./components/App/AppIndex";
+import Firebase, { FirebaseContext } from "./components/firebase/FirebaseIndex";
 
 ReactDOM.render(
   <FirebaseContext.Provider value={new Firebase()}>
