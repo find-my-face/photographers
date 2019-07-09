c6837515adc400a6fe0eb1bd187bf74b61813c58
Y4nolo <yanyee.chan@gmail.com>
2019-07-09 15:53:25 +0100

    added admin page

--- src/components/Admin/AdminIndex.js
+++ src/components/Admin/AdminIndex.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { Component } from "react";
 
-const AdminIndex = () => {
-  return (
-    <div>
-      <h1>hello from AdminIndex</h1>
-    </div>
-  );
-};
+import { withFirebase } from "../Firebase/FirebaseIndex";
 
-export default AdminIndex;
+class AdminPage extends Component {
+  state = {
+    loading: false,
+    users: {}
+  };
+
+  componentDidMount() {
+    this.setState({ loading: true });
+
+    this.props.firebase.users().on("value", snapshot => {
+      this.setState({
+        users: snapshot.val(),
+        loading: false
+      });
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Admin</h1>
+      </div>
+    );
+  }
+}
+export default withFirebase(AdminPage);
