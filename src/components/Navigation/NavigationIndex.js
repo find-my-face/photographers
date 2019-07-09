26fb19344d4334248ed7635c33448c4ffcc9008c
andrew <burns.andrew@hotmail.co.uk>
2019-07-09 12:05:44 +0100

    added signout button to nav

--- src/components/Navigation/NavigationIndex.js
+++ src/components/Navigation/NavigationIndex.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import SignOutButton from "../SignOut/SignOutIndex";
 import * as ROUTES from "../../constants/routes";
 
 const Navigation = () => {
@@ -20,6 +21,9 @@ const Navigation = () => {
         </li>
         <li>
           <Link to={ROUTES.ADMIN}>Admin</Link>
+        </li>
+        <li>
+          <SignOutButton />
         </li>
       </ul>
     </div>
