a32daab22f159d569faf273920c151ee8fb13be8
quinnter <45169425+quinnter@users.noreply.github.com>
2019-07-10 10:15:08 +0100

    Add styling to home (#2)
    * Style app class
    
    * Add css styling to home page

--- src/components/Home/HomeIndex.js
+++ src/components/Home/HomeIndex.js
@@ -2,7 +2,7 @@ import React from "react";
 import { withAuthorisation } from "../Session/SessionIndex";
 
 const HomePage = () => (
-  <div>
+  <div className="App">
     <h1>Home Page</h1>
     <p>The Home Page is accessible by every signed in user.</p>
   </div>
