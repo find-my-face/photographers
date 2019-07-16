50ffef42a72789d7c767807fb2975140772d006e
andrew <burns.andrew@hotmail.co.uk>
2019-07-16 11:19:35 +0100

    mat ui styles now embedded without errors

--- src/components/SignOut/SignOutIndex.js
+++ src/components/SignOut/SignOutIndex.js
@@ -1,25 +1,25 @@
 import React from "react";
 import { withFirebase } from "../firebase/FirebaseIndex";
 import { Button } from "@material-ui/core";
-// import { withStyles } from "@material-ui/core/styles";
+import { withStyles } from "@material-ui/core/styles";
 
-// const styles = theme => ({
-//   root: {
-//     flexGrow: 1,
-//     zIndex: 1,
-//     overflow: "hidden",
-//     position: "relative",
-//     display: "flex",
-//     width: "100%"
-//   },
-//   toolbar: theme.mixins.toolbar,
-//   content: {
-//     flexGrow: 1,
-//     backgroundColor: theme.palette.background.default,
-//     padding: theme.spacing(3),
-//     height: "92vh"
-//   }
-// });
+const styles = theme => ({
+  root: {
+    flexGrow: 1,
+    zIndex: 1,
+    overflow: "hidden",
+    position: "relative",
+    display: "flex",
+    width: "100%"
+  },
+  toolbar: theme.mixins.toolbar,
+  content: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.default,
+    padding: theme.spacing(3),
+    height: "92vh"
+  }
+});
 
 const SignOutButton = ({ firebase }) => (
   <Button
@@ -32,4 +32,4 @@ const SignOutButton = ({ firebase }) => (
   </Button>
 );
 
-export default withFirebase(SignOutButton);
+export default withStyles(styles)(withFirebase(SignOutButton));
